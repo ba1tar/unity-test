138a305da2a77066d5e37cb847c1fcbccf061fc5
mariaclarardrges-maker <baltaryan14@gmail.com>
2026-04-17 16:18:13 -0300

    Unity Aula3
    Lucas

--- Assets/LixoSpawnerController.cs
+++ Assets/LixoSpawnerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LixoSpawnerController : MonoBehaviour
 {
@@ -11,6 +12,8 @@ public class LixoSpawnerController : MonoBehaviour
     public GameObject Lixo;
     public int MaxPoints;
     public int points = 0;
+    public TMP_Text pointsText;
+    
 
     IEnumerator SpawnRoutine() {
         while(points<MaxPoints){
@@ -18,15 +21,10 @@ public class LixoSpawnerController : MonoBehaviour
             yield return new WaitForSeconds(timer);
         }
     }
-    // Start is called before the first frame update
-    void Start()
+    
+    public void AddToPoints(int Value)
     {
-        StartCoroutine(SpawnRoutine());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        points += Value;
+        pointsText.text = "Points: "+points.ToString();
     }
 }
